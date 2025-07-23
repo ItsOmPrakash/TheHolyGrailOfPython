247bd4d662fbbecd0461c1588577f37417a604a9
Om Prakash <omofficials70@gmail.com>
2025-07-23 14:49:56 +0530

    Day-1 Assignment Submission

---
+++ DebugDiaries/001_GreetingFunction.py
@@ -0,0 +1,10 @@
+# Implement a Python program that defines a function named greet_user. 
+# This function should take one argument, name (a string), and print a personalized greeting message 
+# like "Hello, [name]! Welcome to Python." Call this function with a user-provided name.
+#-------------------------------------------------------------
+def greet_user(name):
+   
+    print("Hello,"+ name +"! welcome to Python.")
+
+# calling the sunction and passing the argument
+greet_user(input("enter your name: "))
